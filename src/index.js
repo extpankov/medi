69e30cce7f141f59e82c5f2679a554bfcf324a98
extpankov <RomchanskiyR@yandex.ru>
2021-11-23 01:13:10 +0300

    feat: owl-carousel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    const slider = $(".slider").owlCarousel({
+        loop: true,
+        center: true,
+        nav: true,
+        items: 1,
+        autoWidth: true
+    });
+});
